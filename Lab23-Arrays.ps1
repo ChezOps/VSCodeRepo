b1eaba6537d8ef5e41bd2d5461a691e2e0ca3a90
Chez.Ops <chessari.am@gmail.com>
2023-03-21 22:45:34 +1100

    testingcommit

---
+++ Lab23-Arrays.ps1
@@ -0,0 +1,31 @@
+﻿Set-StrictMode -off
+
+$myArray=@("Test1","Test2","Test3")
+
+$myArray+="Test5"
+
+####################
+## Array Lists ##
+
+# Create the Array List
+$myList = New-Object -TypeName System.Collections.ArrayList
+
+# Use the properties and methods for object info
+$myList.GetType()
+$myList.IsFixedSize
+
+# Add data to the array
+$myList.Add("Test1")
+$myList.AddRange(@("Test2","Test3"))
+$myList.Remove("test2")
+
+# Testing efficiency
+$array=@()
+$arrayList = New-Object -TypeName System.Collections.ArrayList
+
+Measure-Command -Expression {@(0..20000).ForEach({$array+=$_})}
+Measure-Command -Expression {@(0..20000).ForEach({$arrayList.Add($_)})}
+
+##updates
+
+Get-Date
